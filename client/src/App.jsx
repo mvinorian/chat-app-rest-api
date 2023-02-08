5d3403dfcb17beaa3b4be32420babfa7a6ef7843
mvinorian <vinorianmuhammad@gmail.com>
2023-02-08 21:57:09 +0700

    chore: add tailwindcss, axios, and cors dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="font-bold text-blue-500">Test tailwindcss</h1>
+    </div>
+  );
+}
